96c556f1036241fde9f8b78e8a0c73b60051dd08
mabraham-git <120038234+mabraham-git@users.noreply.github.com>
2022-12-07 16:51:22 +0100

    Update code.py

--- Day6/code.py
+++ Day6/code.py
@@ -5,7 +5,7 @@ Task2 = 14
 
 foundit = False
 counter = Task1
-while(foundit == False or counter == len(code)):
+while(foundit == False or counter <= len(code)):
     if(len(set(code[counter-Task1:counter]))==Task1):
         foundit = True
         print(counter)
@@ -14,7 +14,7 @@ while(foundit == False or counter == len(code)):
     
 foundit = False
 counter = Task2
-while(foundit == False or counter == len(code)):
+while(foundit == False or counter <= len(code)):
     if(len(set(code[counter-Task2:counter]))==Task2):
         foundit = True
         print(counter)
