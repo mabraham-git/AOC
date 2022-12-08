77061e9aa62e9019f9383194bce3ef77a247e9c7
mabraham-git <matte.abrahamsson@gmail.com>
2022-12-08 22:20:02 +0100

    Day8

--- Day8/code.py
+++ Day8/code.py
@@ -1,8 +1,50 @@
-# IN DEVELOPMENT!
-
 import numpy as np
 
-input = open("input_small","r")
+def check_view(list, val):  
+    number_of_trees_seen = 0
+    if len(list)==0:
+        return 0
+
+    for x in list: 
+        number_of_trees_seen += 1
+        if val <= x:
+            break
+
+    return number_of_trees_seen
+
+def check_visibility(list, val):  
+    visible = True
+    for x in list:
+        if val <= x:
+            visible = False
+    return visible
+
+def check_tree_view(row,column,matrix):
+    #trees left choosen tree
+    left = check_view(np.flip(matrix[row,:column]),matrix[row][column])
+    #trees right choosen tree
+    right = check_view(matrix[row,column+1:],matrix[row][column])
+    #trees top choosen tree
+    top = check_view(np.flip(matrix[:row,column]),matrix[row][column])
+    #trees below choosen tree
+    below = check_view(matrix[row+1:,column],matrix[row][column])
+
+    return([left,right,top,below])
+
+def check_tree_visibility(row,column,matrix):
+
+    #trees left choosen tree
+    left = check_visibility(matrix[row,:column],matrix[row][column])
+    #trees right choosen tree
+    right = check_visibility(matrix[row,column+1:],matrix[row][column])
+    #trees top choosen tree
+    top = check_visibility(matrix[:row,column],matrix[row][column])
+    #trees below choosen tree
+    below = check_visibility(matrix[row+1:,column],matrix[row][column])
+
+    return [left,right,top,below]
+
+input = open("input","r")
 input_file = input.read().splitlines()
 input.close()
 
@@ -11,21 +53,19 @@ trees = []
 for line in input_file:
     trees.append(list(line))
 
-test = np.array(trees)
+np_trees = np.array(trees)
 
-print(test)
+#Answer 1
+sum = 0
+for idx, x in np.ndenumerate(np_trees):
+    if(any(check_tree_visibility(idx[0],idx[1], np_trees))):
+        sum += 1
+print(sum)
 
-rad = 2
-column = 4
-
-
-
-rad-=1
-column-=1
-#[row][element]
-print("The tree:",test[rad][column])
-print("trees after:",test[rad,column+1:])
-print("trees before:",test[rad,:column])
-
-print("trees under:",test[rad+1:,column])
-print("trees over:",test[:rad,column])
+#Answer 2
+highest_score = 0
+for idx, x in np.ndenumerate(np_trees):
+    left,right,top,below = check_tree_view(idx[0],idx[1], np_trees)
+    if left*right*top*below > highest_score:
+        highest_score = left*right*top*below
+print(highest_score)
